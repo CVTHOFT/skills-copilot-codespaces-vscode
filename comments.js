16b4f2bf15487ce243bf0420e2b76159953df310
chof <chof@PF310PB6>
2023-08-13 15:07:52 +0200

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,97 +1,24 @@
-// create web server
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
+// create web server with express
+const express = require("express");
+const router = express.Router();
+// create comment controller
+const commentController = require("../controllers/comments_controller");
+// create authentication controller
+const passport = require("passport");
+// create post controller
+const postController = require("../controllers/posts_controller");
 
-// set up body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// create comment route
+router.post(
+  "/create",
+  passport.checkAuthentication,
+  commentController.create
+);
+// create comment destroy route
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  commentController.destroy
+);
 
-var port = process.env.PORT || 8080;
-
-// connect to database
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/comments', {useNewUrlParser: true});
-
-// create schema
-var Schema = mongoose.Schema;
-
-var commentSchema = new Schema({
-    name: String,
-    comment: String,
-    timestamp: String
-});
-
-// create model
-var Comment = mongoose.model('Comment', commentSchema);
-
-// create router
-var router = express.Router();
-
-// middleware for all requests
-router.use(function(req, res, next) {
-    console.log('Request received.');
-    next();
-});
-
-// test route
-router.get('/', function(req, res) {
-    res.json({ message: 'API working!' });
-});
-
-// routes that end in /comments
-router.route('/comments')
-    .post(function(req, res) {
-        var comment = new Comment();
-        comment.name = req.body.name;
-        comment.comment = req.body.comment;
-        comment.timestamp = req.body.timestamp;
-
-        comment.save(function(err) {
-            if (err) {
-                res.send(err);
-            }
-            res.json({ message: 'Comment created!' });
-        });
-    })
-    .get(function(req, res) {
-        Comment.find(function(err, comments) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(comments);
-        });
-    });
-
-// routes that end in /comments/:comment_id
-router.route('/comments/:comment_id')
-    .get(function(req, res) {
-        Comment.findById(req.params.comment_id, function(err, comment) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(comment);
-        });
-    })
-    .put(function(req, res) {
-        Comment.findById(req.params.comment_id, function(err, comment) {
-            if (err) {
-                res.send(err);
-            }
-            comment.name = req.body.name;
-            comment.comment = req.body.comment;
-            comment.timestamp = req.body.timestamp;
-
-            comment.save(function(err) {
-                if (err) {
-                    res.send(err);
-                }
-                res.json({ message: 'Comment updated!' });
-            });
-        });
-    })
-    .delete(function(req, res) {
-        Comment.delete 
-
-    });
+module.exports = router;
